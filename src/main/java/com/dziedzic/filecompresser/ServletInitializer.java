d01f83f231dd8d8e5a1ca91e9dc40332796a25f7
Lukasz Dziedzic <ldziedzic1996@gmail.com>
2019-09-17 23:17:49 +0200

    Initial commit

---
+++ src/main/java/com/dziedzic/filecompresser/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.dziedzic.filecompresser;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FilecompresserApplication.class);
+	}
+
+}
